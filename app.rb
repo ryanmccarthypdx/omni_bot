814a6478006d4218fa32d122368d9d8ceb2ee0fc
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-10-09 21:10:59 -0700

    Add basic styling and forms for no_session.erb

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'bundler/setup'
+require 'tilt/erb'
 Bundler.require :default
 Dotenv.load
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get '/' do
+  erb :no_session
+end
+
+post '/login' do
+  redirect '/home'
+end
+
+post '/register' do
+  redirect '/home'
+end
--- spec/features/create_user_path_spec.rb
+++ spec/features/create_user_path_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe "the user creation process" do
 
-  # it "should display the new user form" do
-  #   visit '/'
-  #   expect(page).to have_content "Create a new account"
-  #   expect(page).to have_content "Mobile Phone:"
-  #   expect(page).to have_content "Location:"
-  #   expect(page).to have_content "Password:"
-  #   expect(page).to have_content "Confirm Password:"
-  # end
+  it "should display the new user form" do
+    visit '/'
+    expect(page).to have_content "Create account"
+    expect(page).to have_content "New user sign up"
+    expect(page).to have_content "Mobile Phone:"
+    expect(page).to have_content "Zip Code:"
+    expect(page).to have_content "Password:"
+    expect(page).to have_content "Confirm Password:"
+  end
 end
